6b203a1ce211c96ce0423b36c935a989e08849ef
OdiaChuchi <104531854+OdiaChuchi@users.noreply.github.com>
2022-04-28 02:46:35 +0530

    add tests, add github workflow

---
+++ tests/compilation_test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+for file in `ls ./src`
+    do
+        echo "Running test for $file"
+        if [[ $file == *.py ]]
+            then
+                python3 -m py_compile src/$file
+        elif [[ $file == *.cpp ]]
+            then
+                g++ src/$file
+        elif [[ $file == *.java ]]
+            then
+                javac src/$file
+        elif [[ $file == *.c ]]
+            then
+                gcc src/$file
+        else
+            echo "Unknown file type"
+            return 1
+        fi            
+    done
