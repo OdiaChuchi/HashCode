6bc922ab59ee30190a40d7b7f2357b351d47b4ea
OdiaChuchi <104531854+OdiaChuchi@users.noreply.github.com>
2022-04-28 02:51:45 +0530

    fix tests

--- tests/compilation_test.sh
+++ tests/compilation_test.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-for file in `ls ./src`
+for file in `ls -R .`
     do
         echo "Running test for $file"
         if [[ $file == *.py ]]
             then
-                python3 -m py_compile src/$file
+                python3 -m py_compile $file
         elif [[ $file == *.cpp ]]
             then
-                g++ src/$file
+                g++ $file
         elif [[ $file == *.c ]]
             then
-                gcc src/$file
+                gcc $file
         else
             echo "Unknown file type"
             return 1
